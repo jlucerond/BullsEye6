d60f5e840286093be17345e8f6861c135bc5d609
Joe Lucero <jlucerond@gmail.com>
2017-03-06 22:55:46 -0500

    Game Play Finished

--- BullsEye6/ViewController.swift
+++ BullsEye6/ViewController.swift
@@ -31,15 +31,17 @@ class ViewController: UIViewController {
     
     // MARK: - IBActions
     @IBAction func showAlert() {
-        let title = "You scored \(calculateScore()) points"
+        let title = calculateTitle()
         let message = "The value of the slider was \(currentValue)\nYou were aiming for \(targetValue)"
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "Play Again", style: .default, handler: nil)
+        let action = UIAlertAction(title: "Play Again", style: .default, handler: {action in self.startNewRound()})
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        
-        startNewRound()
+    }
+    
+    @IBAction func restartGame() {
+        startNewGame()
     }
     
 }
@@ -62,7 +64,7 @@ extension ViewController {
     
     func startNewRound() {
         if round != 0 {
-            score += calculateScore()
+            score += calculateScore().modifiedScore
         }
         targetValue = Int(arc4random_uniform(100) + 1)
         slider.value = 50
@@ -70,9 +72,34 @@ extension ViewController {
         updateLabels()
     }
     
-    func calculateScore() -> Int {
+    func calculateTitle() -> String {
+        let simpleScore = calculateScore().simpleScore
+        let modifiedScore = calculateScore().modifiedScore
+        
+        if simpleScore == 100 {
+            return "Perfect 💯: \(modifiedScore) points"
+        } else if simpleScore >= 95 {
+            return "Well done 👏🏽: \(modifiedScore) points"
+        } else if simpleScore >= 90 {
+            return "Ummm 🆗: \(modifiedScore) points"
+        } else {
+            return "Needs work 😕: \(modifiedScore) points"
+        }
+    }
+    
+    func calculateScore() -> (simpleScore: Int, modifiedScore: Int) {
         let offBy = targetValue - currentValue
-        return 100 - abs(offBy)
+        let simpleScore = 100 - abs(offBy)
+        let modifiedScore: Int
+        if simpleScore == 100 {
+            modifiedScore = 200
+        } else if simpleScore >= 95 {
+            modifiedScore = 50 + simpleScore
+        } else {
+            modifiedScore = simpleScore
+        }
+        
+        return (simpleScore, modifiedScore)
     }
     
     func updateLabels() {
